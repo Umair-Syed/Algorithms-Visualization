76e7d5033f0a4d02330cba22cd50088119539b36
KUMARPALAKOLLU <saiganeshpalakollutnl@gmail.com>
2023-01-07 21:00:39 +0530

    add bubb

--- bubble_sort.py
+++ bubble_sort.py
@@ -38,11 +38,9 @@ class Node:
 
 # draw is draw funtion,
 def algorithm(draw,grid, make_bars,rows):
-    swapped = False
     for i in range(len(list)):
         for j in range(0,len(list)-i-1):
             if list[j]>list[j+1]:
-                swapped = True
                 list[j],list[j+1] = list[j+1],list[j]
             make_bars(grid,rows)
             draw()
